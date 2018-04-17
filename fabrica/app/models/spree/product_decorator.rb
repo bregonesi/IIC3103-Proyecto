83533010fbed8c61bd886858bf03ad26348f0441
Benjamin <rego_1994@hotmail.com>
2018-04-17 02:36:48 -0300

    rebuilt can_supply?

---
+++ fabrica/app/models/spree/product_decorator.rb
@@ -0,0 +1,11 @@
+Spree::Product.class_eval do
+  def can_supply?
+  	self.stock_items do |stock_item|
+  		if stock_item.count_on_hand != 0  # apenas se detecte uno entonces se corta
+  			return true
+  		end
+  	end
+
+		return false
+  end
+end
