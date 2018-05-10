2b45355c52450b5e20402fab72c2f238460ee197
Javiera Burgos <jnburgos@uc.cl>
2018-05-10 13:34:58 -0300

    add can_accept method

--- fabrica/app/models/sftp_order.rb
+++ fabrica/app/models/sftp_order.rb
@@ -1,3 +1,17 @@
 class SftpOrder < ApplicationRecord
-  
+
+  def can_accept?
+    variant = Spree::Variant.find_by(sku: self.sku)
+    if variant.total_on_hand >= self.qty
+      return true #se acepta
+    end
+    return false
+  end
+
+
+
+
+
 end
+
+# si tenemos la cantidad del producto solicitado por el pedido, retorna true
