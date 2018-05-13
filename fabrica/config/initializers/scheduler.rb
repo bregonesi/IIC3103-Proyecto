e070e71a6e5d904c45fca8a7e85c8cc03232aaeb
Benjamin <rego_1994@hotmail.com>
2018-05-12 22:22:57 -0300

    trying to fix scheduler

--- fabrica/config/initializers/scheduler.rb
+++ fabrica/config/initializers/scheduler.rb
@@ -3,8 +3,113 @@ require 'rufus-scheduler'
 include SchedulerHelper
 
 
-#if defined?(::Rails::Server) || File.basename($0) =='rake'
-if defined?(::Rails::Server)
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    if forked
+      	job = Rufus::Scheduler.new(:max_work_threads => 1)
+				job.every '35s' do
+				#job.every '1m' do
+				  puts "Ejecutando update."
+
+			    # Marcamos ordenes vencidas como canceladas y las finalizadas como shipped
+			    Scheduler::OrderHelper.marcar_vencidas
+
+					# Aca pagamos las ordenes #
+					Scheduler::PaymentHelper.pagar_ordenes
+
+					# Vemos que ordenes aceptar y cual ignorar (despues las rechazamos) #
+					Scheduler::OrderHelper.aceptar_ordenes
+					Scheduler::OrderHelper.fabricar_api
+
+			    # Cambiamos las ordenes de almacen #
+			    Scheduler::OrderHelper.cambiar_almacen
+
+					# Aca despachamos lo pagado #
+					Scheduler::ShipmentHelper.despachar_ordenes
+
+					# Chequeamos si tenemos nuevos almacenes o nos han eliminado alguno #
+					Scheduler::AlmacenesHelper.nuevos_almacenes
+					Scheduler::AlmacenesHelper.eliminar_extras
+
+					# Aca movemos los items de almacen #
+					Scheduler::ProductosHelper.hacer_movimientos
+
+				  # Cargamos nuevos stocks y stock de almacenes nuevos #
+				  Scheduler::ProductosHelper.cargar_nuevos  ## y elimina los vencidos
+
+				  # Tratamos de que se mantenga los optimos de cada almacen #
+					Scheduler::AlmacenesHelper.mantener_consistencia
+
+				  puts "Termina update."
+				end # end del scheduler
+
+
+			  job_sftp = Rufus::Scheduler.new(:max_work_threads => 1)
+			  job_sftp.every '10m' do
+			    puts "Ejecutando chequeo de ordenes nuevas ftp"
+			    
+			    # Descargamos nuevas ordenes
+			    Scheduler::SftpHelper.agregar_nuevas_ordenes
+
+			    puts "Termina chequeo de ordenes nuevas ftp"
+			  end
+    else
+      # We're in direct spawning mode. We don't need to do anything.
+    end
+  end
+else # not Passenger, probably Webrick in dev mode
+  	job = Rufus::Scheduler.new(:max_work_threads => 1)
+	job.every '35s' do
+	#job.every '1m' do
+	  puts "Ejecutando update."
+
+    # Marcamos ordenes vencidas como canceladas y las finalizadas como shipped
+    Scheduler::OrderHelper.marcar_vencidas
+
+		# Aca pagamos las ordenes #
+		Scheduler::PaymentHelper.pagar_ordenes
+
+		# Vemos que ordenes aceptar y cual ignorar (despues las rechazamos) #
+		Scheduler::OrderHelper.aceptar_ordenes
+		Scheduler::OrderHelper.fabricar_api
+
+    # Cambiamos las ordenes de almacen #
+    Scheduler::OrderHelper.cambiar_almacen
+
+		# Aca despachamos lo pagado #
+		Scheduler::ShipmentHelper.despachar_ordenes
+
+		# Chequeamos si tenemos nuevos almacenes o nos han eliminado alguno #
+		Scheduler::AlmacenesHelper.nuevos_almacenes
+		Scheduler::AlmacenesHelper.eliminar_extras
+
+		# Aca movemos los items de almacen #
+		Scheduler::ProductosHelper.hacer_movimientos
+
+	  # Cargamos nuevos stocks y stock de almacenes nuevos #
+	  Scheduler::ProductosHelper.cargar_nuevos  ## y elimina los vencidos
+
+	  # Tratamos de que se mantenga los optimos de cada almacen #
+		Scheduler::AlmacenesHelper.mantener_consistencia
+
+	  puts "Termina update."
+	end # end del scheduler
+
+
+  job_sftp = Rufus::Scheduler.new(:max_work_threads => 1)
+  job_sftp.every '10m' do
+    puts "Ejecutando chequeo de ordenes nuevas ftp"
+    
+    # Descargamos nuevas ordenes
+    Scheduler::SftpHelper.agregar_nuevas_ordenes
+
+    puts "Termina chequeo de ordenes nuevas ftp"
+  end
+end
+=begin
+if defined?(::Rails::Server) || File.basename($0) =='rake'
+#if defined?(::Rails::Server)
+
 	puts "Partiendo scheduler"
 
 	job = Rufus::Scheduler.new(:max_work_threads => 1)
@@ -56,3 +161,4 @@ if defined?(::Rails::Server)
   end
 
 end
+=end
