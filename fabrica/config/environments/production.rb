e6ac62649cb2a0242ef6f40d413781208ec257c6
Benjamin <rego_1994@hotmail.com>
2018-05-12 23:54:07 -0300

    change urls prod

--- fabrica/config/environments/production.rb
+++ fabrica/config/environments/production.rb
@@ -90,18 +90,18 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # API url
-  ENV['api_url'] = "https://integracion-2018-dev.herokuapp.com/"  ## development
-  # ENV['api_url'] = "https://integracion-2018-prod.herokuapp.com/"  ## production
+  #ENV['api_url'] = "https://integracion-2018-dev.herokuapp.com/"  ## development
+  ENV['api_url'] = "https://integracion-2018-prod.herokuapp.com/"  ## production
   ENV['api_psswd'] = "TGVaa:#Dtih:kx4"
 
   # API OC url
-  ENV['api_oc_url'] = "https://integracion-2018-dev.herokuapp.com/oc/"
-  # ENV['api_oc_url'] = "https://integracion-2018-prod.herokuapp.com/oc/"  ## production
+  #ENV['api_oc_url'] = "https://integracion-2018-dev.herokuapp.com/oc/"  ## development
+  ENV['api_oc_url'] = "https://integracion-2018-prod.herokuapp.com/oc/"  ## production
 
   # SFTP de ordenes mayorias
-  ENV['sftp_ordenes_url'] = "integradev.ing.puc.cl"  ## development
-  #ENV['sftp_ordenes_url'] = "integracion.ing.puc.cl"  ## production
+  #ENV['sftp_ordenes_url'] = "integradev.ing.puc.cl"  ## development
+  ENV['sftp_ordenes_url'] = "integracion.ing.puc.cl"  ## production
   ENV['sftp_ordenes_login'] = "grupo4"
-  ENV['sftp_ordenes_psswd'] = "1ccWcVkAmJyrOfA"  ## development
-  #ENV['sftp_ordenes_psswd'] = "U81Y9W1umtDDvON2w"  ## production
+  #ENV['sftp_ordenes_psswd'] = "1ccWcVkAmJyrOfA"  ## development
+  ENV['sftp_ordenes_psswd'] = "U81Y9W1umtDDvON2w"  ## production
 end
