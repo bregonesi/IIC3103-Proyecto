9a8bb0d5c77c583a68064538d6a2601d782e3e8f
Benjamin <rego_1994@hotmail.com>
2018-06-03 16:09:05 -0400

    comment scheduler

--- fabrica/config/initializers/scheduler.rb
+++ fabrica/config/initializers/scheduler.rb
@@ -23,28 +23,28 @@ if defined?(::Rails::Server) || defined?(PhusionPassenger)
 			Scheduler::ProductosHelper.cargar_nuevos  ## y elimina los vencidos
 			
 			# Vemos que ordenes aceptar #
-			Scheduler::OrderHelper.aceptar_ordenes
+			#Scheduler::OrderHelper.aceptar_ordenes
 
 			# Chequeo de si alguna de las aceptadas tiene stock #
-			Scheduler::OrderHelper.chequear_si_hay_stock
+			#Scheduler::OrderHelper.chequear_si_hay_stock
 
 			# Aca pagamos las ordenes #
 			Scheduler::PaymentHelper.pagar_ordenes
 
 			# Aca despachamos lo pagado #
-			Scheduler::ShipmentHelper.despachar_ordenes
+			#Scheduler::ShipmentHelper.despachar_ordenes
 
 			# Cambiamos las ordenes de almacen #
-			Scheduler::OrderHelper.cambiar_almacen
+			#Scheduler::OrderHelper.cambiar_almacen
 
 			# Aca despachamos lo pagado #
-			Scheduler::ShipmentHelper.despachar_ordenes
+			#Scheduler::ShipmentHelper.despachar_ordenes
 
 			# Volvemos a sincronizar ya que pudimos haber despachado #
-			Scheduler::OrderHelper.sincronizar_informacion
+			#Scheduler::OrderHelper.sincronizar_informacion
 			
 			# Fabricamos las pedidas #
-			Scheduler::OrderHelper.fabricar_api
+			#Scheduler::OrderHelper.fabricar_api
 			
 			# Chequeamos si tenemos nuevos almacenes o nos han eliminado alguno #
 			Scheduler::AlmacenesHelper.nuevos_almacenes
