c4aa503c078953af98d3320bbd03d0f107393a78
Benjamin <rego_1994@hotmail.com>
2018-06-02 21:02:23 -0400

    fix en fabricar

--- fabrica/app/helpers/scheduler/order_helper.rb
+++ fabrica/app/helpers/scheduler/order_helper.rb
@@ -393,7 +393,10 @@ module Scheduler::OrderHelper
 						stock_movement = almacen_despacho.stock_movements.build(quantity: -ingredient.amount.to_i)
 						stock_movement.action = "Mandamos a fabricar."
 						stock_movement.stock_item = almacen_despacho.set_up_stock_item(ingredient.variant_ingredient)
-						Scheduler::ProductosHelper.cargar_detalles(stock_location.stock_items.find_by(variant: ingredient.variant_ingredient))
+
+						stock_item_ingrediente = almacen_despacho.stock_items.find_by(variant: ingredient.variant_ingredient)
+						obtener_lote_antiguo(stock_item_ingrediente, cantidad=ingredient.amount.to_i).destroy_all
+						Scheduler::ProductosHelper.cargar_detalles(stock_item_ingrediente)
 					end
 
 					body = JSON.parse(r.body)
