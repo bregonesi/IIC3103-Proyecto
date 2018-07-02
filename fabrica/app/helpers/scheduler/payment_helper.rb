30449d94a920b13721955587d559cc6b023978e9
Benjamin <rego_1994@hotmail.com>
2018-07-02 16:35:52 -0400

    fix

--- fabrica/app/helpers/scheduler/payment_helper.rb
+++ fabrica/app/helpers/scheduler/payment_helper.rb
@@ -6,14 +6,14 @@ module Scheduler::PaymentHelper
 		# Aca pagamos las ordenes #
 		Spree::Order.where(payment_state: "balance_due").each do |orden_inpaga|
 			orden_inpaga.with_lock do
-				if orden_inpaga.canal == "b2b"
-					factura = Invoice.find_by(originator_type: orden_inpaga.class.name.to_s, originator: orden_inpaga.id.to_i)
+				if orden_inpaga.sftp_order.canal == "b2b"
+					factura = Invoice.find_by(originator_type: orden_inpaga.sftp_order.class.name.to_s, originator: orden_inpaga.sftp_order.id.to_i)
 					if factura.nil?
-						puts "No tenemos factura para la sftp order b2b " + orden_inpaga.id.to_s
+						puts "No tenemos factura para la sftp order b2b " + orden_inpaga.sftp_order.id.to_s
 						next
 					else
 						if !factura.estado == "pagada"
-							puts "Factura para sftp order " + orden_inpaga.id.to_s + " no se encuentra pagada."
+							puts "Factura para sftp order " + orden_inpaga.sftp_order.id.to_s + " no se encuentra pagada."
 							next
 						end
 					end
