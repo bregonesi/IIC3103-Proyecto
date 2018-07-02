7fdbed44af3e5c8542ba4685ff80c0de4012408e
Benjamin <rego_1994@hotmail.com>
2018-07-02 16:31:43 -0400

    chequeo si me han pagado

--- fabrica/app/helpers/scheduler/payment_helper.rb
+++ fabrica/app/helpers/scheduler/payment_helper.rb
@@ -6,16 +6,24 @@ module Scheduler::PaymentHelper
 		# Aca pagamos las ordenes #
 		Spree::Order.where(payment_state: "balance_due").each do |orden_inpaga|
 			orden_inpaga.with_lock do
-				#if orden_inpaga.payments.first.number == "algo"  # aqui se chequea que coincide el pago
-					#orden_inpaga.payment_state = "paid"
-					#orden_inpaga.outstanding_balance
-					orden_inpaga.payments.where(state: "checkout").each do |orden_inpaga_pagos|
-						puts "Marcando payment " + orden_inpaga_pagos.number.to_s + " como pagado"
-						orden_inpaga_pagos.send("capture!")  ## con esto se marca como pagado y se registra
+				if orden_inpaga.canal == "b2b"
+					factura = Invoice.find_by(originator_type: orden_inpaga.class.name.to_s, originator: orden_inpaga.id.to_i)
+					if factura.nil?
+						puts "No tenemos factura para la sftp order b2b " + orden_inpaga.id.to_s
+						next
+					else
+						if !factura.estado == "pagada"
+							puts "Factura para sftp order " + orden_inpaga.id.to_s + " no se encuentra pagada."
+							next
+						end
 					end
-					# probablemente tengamos que llamar a la api del profe
-					puts "Orden cambia a estado pagada"
-				# end
+				end
+
+				orden_inpaga.payments.where(state: "checkout").each do |orden_inpaga_pagos|
+					puts "Marcando payment " + orden_inpaga_pagos.number.to_s + " como pagado"
+					orden_inpaga_pagos.send("capture!")  ## con esto se marca como pagado y se registra
+				end
+				puts "Orden cambia a estado pagada"
 			end
 		end
 	end
